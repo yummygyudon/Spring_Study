cef256a252bc8a131825dec07fa654f13fa40143
gyudon <duck9912@naver.com>
2022-07-11 21:38:11 +0900

    2022.07.01 : inflearn_servlet start

---
+++ Inflearn/servlet/src/main/groovy/hello/servlet/ServletApplication.groovy
@@ -0,0 +1,13 @@
+package hello.servlet
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServletApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ServletApplication, args)
+	}
+
+}
